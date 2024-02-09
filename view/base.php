4000d54165ef4d835557ec9a407622e7cdb2cf45
OmerAticii <Atc.pro@myyahoo.com>
2024-02-09 15:32:40 +0100

    push avant merge 09/02/2024

--- view/base.php
+++ view/base.php
@@ -15,7 +15,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a target="_self" href="index.php"><img src="img/Elitcar.png" alt="Logo" width="110px" height="27px" class="d-inline-block align-text-top"></a>
+    <a target="_self" href="index.php"><img src="../img/header/Elitcar.png" alt="Logo" width="110px" height="27px" class="d-inline-block align-text-top"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
