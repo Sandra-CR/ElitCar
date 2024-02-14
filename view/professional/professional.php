a4682aeb05c00e5bcf626cdce841b56bf7d8c31d
Mhite55 <pierreguyot55@gmail.com>
2024-02-14 17:47:08 +0100

    Page fini manque peaufinage 14/02/2024

--- view/professional/professional.php
+++ view/professional/professional.php
@@ -18,55 +18,297 @@ include_once"../base.php";
             </div>
         </div>
         <div class="container-pro-img h-100 col-5 mt-4 ">
-            <div class="container-pro-img-1 col-8 h-100 bg-success mx-auto"></div>
+            <div class="container-pro-img-1 col-8 h-100 mx-auto"></div>
         </div>
     </div>
+
     <div class="container-professional-2 row col-12 g-0 ">
+    
         <div class="container-main-choose-sub h-75 col-10 mx-auto">
-            <div class="container-main-choose-sub-title h-25 bg-success col-7 mt-5">
+
+            <div class="container-main-choose-sub-title h-25  col-7 mt-5">
                 <h2>Faites augmenter votre chiffre d’affaire d’une manière significative</h2>
             </div>
-            <div class="container-main-choose-sub-card h-75 bg-primary col-12">
-                <div class="container-main-choose-sub-card-1 col-3 h-75 bg-warning">
+
+            <div class="container-main-choose-sub-card h-75 col-12">
+
+                <div class="container-main-choose-sub-card-1 col-3 h-75  ">
+
                     <div class="container-main-choose-sub-card-title mt-4">
+
                         <p>Vous avez moins de 10 voitures</p>
+
                     </div>
                     <div class="container-main-choose-sub-card-price mt-3">
+
                         <h5>12000 CFA par an</h5>
                     </div>
+
                     <div class="container-main-choose-sub-card-btn">
+
                         <a target="_self" href="view/professional/create_professional">
+
                             <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
+
                     </div>
+
                 </div>
-                <div class="container-main-choose-sub-card-2 col-3 h-75 bg-danger">
+
+                <div class="container-main-choose-sub-card-2 col-3 h-75 ">
+
                     <div class="container-main-choose-sub-card-title mt-4">
                         <p>Vous avez moins de 10 voitures</p>
                     </div>
+
                     <div class="container-main-choose-sub-card-price mt-3">
                         <h5>12000 CFA par an</h5>
                     </div>
+
                     <div class="container-main-choose-sub-card-btn ">
                         <a target="_self" href="view/professional/create_professional">
                             <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
                     </div>
+
                 </div>
-                <div class="container-main-choose-sub-card-3 col-3 h-75 bg-secondary">
+
+                <div class="container-main-choose-sub-card-3 col-3 h-75 ">
+
                     <div class="container-main-choose-sub-card-title mt-4">
                         <p>Vous avez moins de 10 voitures</p>
                     </div>
+
                     <div class="container-main-choose-sub-card-price mt-3">
                         <h5>12000 CFA par an</h5>
                     </div>
+
                     <div class="container-main-choose-sub-card-btn">
                         <a target="_self" href="view/professional/create_professional">
                             <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
                     </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container-professional-3 row col-12 g-0 "></div>
+        <!-- Section FAQ -->
+        <div class="container col-10 my-5">
+            <h2 class="text-FAQ mb-4">Foire aux questions</h2>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                    <!-- First accordion item -->
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Second column of accordions -->
+                <div class="col-md-6">
+                <!-- Second accordion item -->
+                    <div class="accordion" id="accordionExample2">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                    <!-- First accordion item -->
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Second column of accordions -->
+                <div class="col-md-6">
+                    <!-- Second accordion item -->
+                    <div class="accordion" id="accordionExample2">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                    <!-- First accordion item -->
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Second column of accordions -->
+                <div class="col-md-6">
+                    <!-- Second accordion item -->
+                    <div class="accordion" id="accordionExample2">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor?
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+                                <div class="accordion-body">
+                                Contenu ...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                <!-- First accordion item -->
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        Contenu ...
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <!-- Second column of accordions -->
+                <div class="col-md-6">
+                <!-- Second accordion item -->
+                <div class="accordion" id="accordionExample2">
+                    <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+                        <div class="accordion-body">
+                        Contenu ...
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                <!-- First accordion item -->
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        Contenu ...
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <!-- Second column of accordions -->
+                <div class="col-md-6">
+                <!-- Second accordion item -->
+                <div class="accordion" id="accordionExample2">
+                    <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+                        <div class="accordion-body">
+                        Contenu ...
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- First column of accordions -->
+                <div class="col-md-6">
+                <!-- First accordion item -->
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit, sed do eiusmod tempor?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        Contenu ...
+                        </div>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
     </div>
 </main>
+<?php include_once "../footer.php"; ?>
