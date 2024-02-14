189f3e762735bb8e4a437ddfe0ce27fcb0d2d68a
Mhite55 <pierreguyot55@gmail.com>
2024-02-14 14:54:22 +0100

    2 section 14/02/2024

--- view/professional/professional.php
+++ view/professional/professional.php
@@ -13,7 +13,7 @@ include_once"../base.php";
                     <p>Choisir le forfait qui correspond à la taille de votre agence est une étape cruciale pour garantir une connectivité efficace et une communication sans heurts.</p>
                 </div>
                 <div class="container-btn-create">
-                    <a target="_self" href="view/professional/create_professional"><button class="btn btn-warning my-2">S'inscrire</button></a>
+                    <a target="_self" href="view/professional/create_professional"><button class="btn btn-warning-pro-1 my-2">S'inscrire</button></a>
                 </div>
             </div>
         </div>
@@ -22,47 +22,47 @@ include_once"../base.php";
         </div>
     </div>
     <div class="container-professional-2 row col-12 g-0 ">
-        <div class="container-main-choose-sub h-75 col-8 mx-auto">
+        <div class="container-main-choose-sub h-75 col-10 mx-auto">
             <div class="container-main-choose-sub-title h-25 bg-success col-7 mt-5">
                 <h2>Faites augmenter votre chiffre d’affaire d’une manière significative</h2>
             </div>
             <div class="container-main-choose-sub-card h-75 bg-primary col-12">
-                <div class="container-main-choose-sub-card-1 col-3 h-50 bg-warning">
-                    <div class="container-main-choose-sub-card-title mt-1">
+                <div class="container-main-choose-sub-card-1 col-3 h-75 bg-warning">
+                    <div class="container-main-choose-sub-card-title mt-4">
                         <p>Vous avez moins de 10 voitures</p>
                     </div>
-                    <div class="container-main-choose-sub-card-price">
+                    <div class="container-main-choose-sub-card-price mt-3">
                         <h5>12000 CFA par an</h5>
                     </div>
                     <div class="container-main-choose-sub-card-btn">
                         <a target="_self" href="view/professional/create_professional">
-                            <button class="btn btn-warning my-2">S'inscrire</button>
+                            <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
                     </div>
                 </div>
-                <div class="container-main-choose-sub-card-2 col-3 h-50 bg-danger">
-                    <div class="container-main-choose-sub-card-title mt-1">
+                <div class="container-main-choose-sub-card-2 col-3 h-75 bg-danger">
+                    <div class="container-main-choose-sub-card-title mt-4">
                         <p>Vous avez moins de 10 voitures</p>
                     </div>
-                    <div class="container-main-choose-sub-card-price">
+                    <div class="container-main-choose-sub-card-price mt-3">
                         <h5>12000 CFA par an</h5>
                     </div>
-                    <div class="container-main-choose-sub-card-btn">
+                    <div class="container-main-choose-sub-card-btn ">
                         <a target="_self" href="view/professional/create_professional">
-                            <button class="btn btn-warning my-2">S'inscrire</button>
+                            <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
                     </div>
                 </div>
-                <div class="container-main-choose-sub-card-3 col-3 h-50 bg-secondary">
-                    <div class="container-main-choose-sub-card-title mt-1">
+                <div class="container-main-choose-sub-card-3 col-3 h-75 bg-secondary">
+                    <div class="container-main-choose-sub-card-title mt-4">
                         <p>Vous avez moins de 10 voitures</p>
                     </div>
-                    <div class="container-main-choose-sub-card-price">
+                    <div class="container-main-choose-sub-card-price mt-3">
                         <h5>12000 CFA par an</h5>
                     </div>
                     <div class="container-main-choose-sub-card-btn">
                         <a target="_self" href="view/professional/create_professional">
-                            <button class="btn btn-warning-pro col-8 my-2">S'inscrire</button>
+                            <button class="btn btn-warning-pro-2  mt-5">S'inscrire</button>
                         </a>
                     </div>
                 </div>
