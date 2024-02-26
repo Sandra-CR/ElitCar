67c50128b8ae6466817c2096f2faeb4883243ef2
Azd Aljayouch <156325800+Dza67@users.noreply.github.com>
2024-02-26 09:55:49 +0100

    ajout fonctionnalité UPDATE de la base de donnée

--- controller/admin/ajax_num.php
+++ controller/admin/ajax_num.php
@@ -2,27 +2,40 @@
 
 include_once "../../model/pdo.php";
 
-if (!empty($_POST["numero"])) {
+if (!empty($_POST["id_user"]) && !empty($_POST["numero"])) {
+    $id_user = $_POST["id_user"];
     $numero = $_POST["numero"];
 
     try {
-        $sql = "INSERT INTO particular (num_mobile) VALUES (?)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$numero]);
-        $response = [
-            "status" => "success",
-            "message" => "Compte créé"
-        ];
+        
+        $check_user_sql = "SELECT * FROM particular WHERE id_user = ?";
+        $check_stmt = $pdo->prepare($check_user_sql);
+        $check_stmt->execute([$id_user]);
+        $user = $check_stmt->fetch(PDO::FETCH_ASSOC);
 
+        if ($user) {
+            
+            $update_sql = "UPDATE particular SET num_mobile = ? WHERE id_user = ?";
+            $update_stmt = $pdo->prepare($update_sql);
+            $update_stmt->execute([$numero, $id_user]);
+            $response = [
+                "status" => "success",
+                "message" => "Numéro mis à jour avec succès"
+            ];
+        } else {
+            
+            $response = [
+                "status" => "failed",
+                "message" => "L'utilisateur avec l'id spécifié n'existe pas dans la base de données"
+            ];
+        }
     } catch (Exception $error) {
         $response = [
-            "status" => "Numéro inséré avec succès",
+            "status" => "failed",
             "message" => "Problème de base de données. Contactez immédiatement un administrateur !"
         ];
     }
-
 } else {
-    
     $response = [
         "status" => "failed",
         "message" => "Veuillez fournir un numéro valide"
