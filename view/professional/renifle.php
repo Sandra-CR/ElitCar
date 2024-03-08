f10b4113fa4315d9a6a4f748d9eaa58b3e1653c1
Zakaria77zkr <zakariahaddaoui60@gmail.com>
2024-03-08 14:10:36 +0100

    style de add_car

---
+++ view/professional/renifle.php
@@ -0,0 +1,104 @@
+<h4>Options du véhicule</h4>             
+                
+                <!-- Option Bluetooth -->
+                <ul>
+                    <li>
+                        <input type="checkbox" value="Bluetooth" class="options"onchange="showRecap()">
+                        <label for="">Bluetooth</label>
+                    </li>
+                <!-- Option Climatisation -->
+                    <li>
+                        <input type="checkbox" value="Climatisation"class="options" onchange="showRecap()"> 
+                        <label for="">Climatisation</label>
+                    </li>
+                <!-- Option GPS -->
+                    <li>
+                        <input type="checkbox" value="GPS" class="options" onchange="showRecap()">
+                        <label for="">GPS</label>
+                    </li>
+                <!-- Option Carplay, Android Auto -->
+                    <li>
+                        <input type="checkbox" value="Carplay" class="options" onchange="showRecap()">
+                        <label for="">Carplay</label>
+                    </li>
+                <!-- Option Attache ISOFIX -->
+                    <li>
+                        <input type="checkbox" value="Attache ISOFIX" class="options" onchange="showRecap()">
+                        <label for="">Attache ISOFIX</label>
+                    </li>
+                <!-- Option Caméra de recul -->
+                    <li>
+                        <input type="checkbox" value="Caméra de recul" class="options" onchange="showRecap()">
+                        <label for="">Caméra de recul</label>
+                    </li>
+                <!-- Option Siège pour enfant -->    
+                    <li>
+                        <input type="checkbox" value="Siège pour enfant" class="options" onchange="showRecap()">
+                        <label for="">Siège pour enfant</label>
+                    </li>
+                    <!-- Option Toit ouvrant -->    
+                    <li>
+                        <input type="checkbox" value="Toit ouvrant" class="options" onchange="showRecap()">
+                        <label for="">Toit ouvrant</label>
+                    </li>
+                    <!-- Option Port USB -->    
+                    <li>
+                        <input type="checkbox" value="Port USB" class="options" onchange="showRecap()">
+                        <label for="">Port USB</label>
+                    </li>
+                    <!-- Option Porte-vélos -->    
+                    <li>
+                        <input type="checkbox" value="Porte-vélos" class="options" onchange="showRecap()">
+                        <label for="">Porte-vélos</label>
+                    </li>
+                    <!-- Option Décapotable -->    
+                    <li>
+                        <input type="checkbox" value="Décapotable" class="options" onchange="showRecap()">
+                        <label for="">Décapotable</label>
+                    </li>
+                    <!-- Option Android Auto -->    
+                    <li>
+                        <input type="checkbox" value="Android Auto" class="options" onchange="showRecap()">
+                        <label for="">Android Auto</label>
+                    </li>
+                    <!-- Option Entrée auxiliaire -->    
+                    <li>
+                        <input type="checkbox" value="Entrée auxiliaire" class="options" onchange="showRecap()">
+                        <label for="">Entrée auxiliaire</label>
+                    </li>
+                    <!-- Option Surveillance des angles morts -->    
+                    <li>
+                        <input type="checkbox" value="Surveillance des angles morts" class="options" onchange="showRecap()">
+                        <label for="">Surveillance des angles morts</label>
+                    </li>
+                    <!-- Option Sièges chauffants -->    
+                    <li>
+                        <input type="checkbox" value="Sièges chauffants" class="options" onchange="showRecap()">
+                        <label for="">Sièges chauffants</label>
+                    </li>
+                    <!-- Option Chargeur USB -->    
+                    <li>
+                        <input type="checkbox" value="Chargeur USB" class="options" onchange="showRecap()">
+                        <label for="">Chargeur USB</label>
+                    </li>
+                    <!-- Option Carte de péage -->
+                    <li>
+                        <input type="checkbox" value="Carte de péage" class="options" onchange="showRecap()">
+                        <label for="">Carte de péage</label>
+                    </li>
+                     <!-- Option Traction intégrale -->
+                     <li>
+                        <input type="checkbox" value="Traction intégrale" class="options" onchange="showRecap()">
+                        <label for="">Traction intégrale</label>
+                    </li>
+                    <!-- Option Accessible en fauteuil roulant -->
+                    <li>
+                        <input type="checkbox" value="Accessible en fauteuil roulant" class="options" onchange="showRecap()">
+                        <label for="">Accessible en fauteuil roulant</label>
+                    </li>
+                    <!-- Option Accès sans clé -->
+                    <li>
+                        <input type="checkbox" value="Accès sans clé" class="options" onchange="showRecap()">
+                        <label for="">Accès sans clé</label>
+                    </li>
+                </ul>
