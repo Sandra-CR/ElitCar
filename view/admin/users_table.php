a2c204513f8ba9c01245116fd50d31aad70718e2
MAROUA <dihabi.maroua1@gmail.com>
2024-03-11 10:27:23 +0100

    dashboard_admin front

---
+++ view/admin/users_table.php
@@ -0,0 +1,5 @@
+<!-- dashboard -->
+<?php
+ include_once "../../controller/admin/role.php"; 
+ include_once "../include/base.php";
+ include_once "../include/admin/dashboard_admin.php";?>
---
+++ view/include/admin/dashboard_admin.php
@@ -0,0 +1,38 @@
+
+<div class="fluid-container dashboard-fluid-container mt-3">
+  <ul class="nav nav-tabs dashboard-nav-tabs ">
+    <li class="nav-item nav-tem-left">
+      <a class="nav-link dashboard-nav-link dashboard-nav-link-left 
+      <?php 
+        $current_file = basename($_SERVER['SCRIPT_NAME']);
+        echo ($current_file == 'advertisements.php') ? 'active-link' : ''; 
+      ?>" 
+      aria-current="page" href="view/admin/advertisements.php" >LES ANNONCES</a>
+    </li>
+    <li class="nav-item nav-item-right">
+      <a class="nav-link dashboard-nav-link dashboard-nav-link-right
+      <?php 
+        $current_file = basename($_SERVER['SCRIPT_NAME']);
+        echo ($current_file == 'notifications.php') ? 'active-link' : ''; 
+      ?>
+    " aria-current="page" href="view/admin/notifications.php">MES NOTIFICATIONS</a>
+    </li>
+    <li class="nav-item nav-item-right">
+      <a class="nav-link dashboard-nav-link dashboard-nav-link-right
+      <?php 
+        $current_file = basename($_SERVER['SCRIPT_NAME']);
+        echo ($current_file == 'messages.php') ? 'active-link' : ''; 
+      ?>
+    " aria-current="page" href="view/admin/messages.php">MES MESSAGES</a>
+    </li>
+    <li class="nav-item nav-item-right">
+      <a class="nav-link dashboard-nav-link dashboard-nav-link-right
+      <?php 
+        $current_file = basename($_SERVER['SCRIPT_NAME']);
+        echo ($current_file == 'users_table.php') ? 'active-link' : ''; 
+      ?>
+    " aria-current="page" href="view/admin/users_table.php">TABLEAU DES UTILISATEURS</a>
+    </li>
+  </ul>
+</div> 
+
